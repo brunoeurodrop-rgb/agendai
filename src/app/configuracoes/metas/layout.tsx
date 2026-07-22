5906591fd0852e003d86793dea3b7b7a5108feef
Bruno Pimenta <bkpimenta81@gmail.com>
2026-07-22 19:06:20 -0300

    fix layout aninhado metas

--- src/app/configuracoes/metas/layout.tsx
+++ src/app/configuracoes/metas/layout.tsx
@@ -1,4 +1,3 @@
-import DashboardLayout from '@/app/dashboard/layout'
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <DashboardLayout>{children}</DashboardLayout>
+  return <>{children}</>
 }
